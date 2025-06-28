529e74650582f1f34c0b13ce85d3c03433b41343
directlink-opener <kgediya0898@gmail.com>
2025-06-28 15:12:01 +0530

    Improved ReadMe

--- Cache/TypeScript/Src/Assets/Bitmoji 3D.lsc/Modules/Bitmoji3DOverrideInterface.ts
+++
@@ -1,7 +0,0 @@
-export interface Bitmoji3DOptionsOverride extends ScriptComponent {
-  modifyBitmoji3DOptions: (options: Bitmoji3DOptions) => Bitmoji3DOptions;
-}
-
-export interface Bitmoji3DAvatarOverride extends ScriptComponent {
-  modifyBitmoji3DAvatar: (bitmoji: { getUser(), getAvatar() }) => void;
-}
--- Cache/TypeScript/Src/Packages/SpectaclesInteractionKit.lspkg/VersionNumber.ts
+++
@@ -1,4 +0,0 @@
-export const VersionNumberMajor = 0
-export const VersionNumberMinor = 13
-export const VersionNumberPatch = 0
-export const VersionNumber = "0.13.0"
